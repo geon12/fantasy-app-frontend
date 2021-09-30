fd9e32843695d9f855eadec3f44be330cdd6bb5f
Cameron <camcreel@gmail.com>
2021-09-30 19:04:22 -0400

    Add Login component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  //const [user,setUser] = useState(null)
+  // const newUserData = {
+  //   "username": "icet",
+  //   "email" : "ice@gmail.com",
+  //   "password": "kobebryant"
+  // }
+  // useEffect(() =>{
+  //   fetch("http://localhost:3000/signup", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //     body: JSON.stringify(newUserData),
+  //   })
+  //     .then((r) => r.json())
+  //     .then((data) => {
+  //       // save the token to localStorage for future access
+  //       localStorage.setItem("jwt", data.jwt);
+  //       // save the user somewhere (in state!) to log the user in
+  //       //setUser(data.user);
+  //       console.log(data)
+  //     });
+  // },[])
   return (
     <div>
       
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div>
+             <h1>Login to your Fantasy Account</h1>
+        </div>
+    )
+}
+
+export default Login
